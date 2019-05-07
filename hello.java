174996e826bb4203242ba290178f3a6038e3df34
harold <uzzal17.ict@gmail.com>
2019-05-07 12:30:01 -0600

    added a new method called display hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@ public class hello{
     public static void main(String[] args){
         System.out.println('hello');
     }
+    greeting('harold')
 
+}
+
+public static void greeting(String name){
+    System.out.println('Welcoome '+ name)
 }
